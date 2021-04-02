a7baa5d35bcaaec1c3a0a083efa2886f19294072
reisap <r.prasaptaraya@gmail.com>
2021-04-02 22:25:03 +0700

    update site Fri Apr  2 22:25:03 WIB 2021

--- main.go
+++ main.go
@@ -12,9 +12,38 @@ import (
 
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	router := gin.Default()
-	router.GET("/",handler)
-	router.Run()
+	//router := gin.Default()
+	//router.GET("/",handler)
+	//router.Run()
+	dsn := "root:Reisa30041989@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db , err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "dari service"
+	userInput.Password= "halo bro"
+	userInput.Occupation ="Petani"
+	userInput.Email = "petani@gmail.com"
+
+	userService.RegisterUser(userInput)
+
+
+	//contoh cek tanpa service
+	//user := user.User{
+	//	Name:"gorun",
+	//}
+	//userRepository.Save(user)
+
+	//input
+	//handler
+	//services : melakukan mapping dari struct user ke struct repository
+	//repository
+	//db
 }
 
 func handler(c *gin.Context){
