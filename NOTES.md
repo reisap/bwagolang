885f3995904de0091a2633e93006b6edad043a57
reisap <r.prasaptaraya@gmail.com>
2021-04-02 21:36:04 +0700

    bagian 1

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+```Learn from bwa```
+
+Urutan Mapping Code:
+1. handler
+2. Services
+3. Repository
+4. Database
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
+
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"golangbwa/user"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 )
 
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	router := gin.Default()
+	router.GET("/",handler)
+	router.Run()
+}
+
+func handler(c *gin.Context){
 	dsn := "root:Reisa30041989@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db , err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -19,11 +28,11 @@ func main() {
 	var users []user.User
 	db.Find(&users)
 
-	length := len(users)
-	fmt.Println(length)
-
 	for _,user := range(users){
 		fmt.Println(user.Email)
 		fmt.Println(user.Name)
 	}
+
+	c.JSON(http.StatusOK,users)
+
 }
