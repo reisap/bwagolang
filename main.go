412a4bea69966bbce6fa0a0579b5999470171e66
reisap <r.prasaptaraya@gmail.com>
2021-04-02 22:49:47 +0700

    update site Fri Apr  2 22:49:47 WIB 2021

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ import (
 )
 
 func main() {
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	//router := gin.Default()
-	//router.GET("/",handler)
-	//router.Run()
 	dsn := "root:Reisa30041989@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db , err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
