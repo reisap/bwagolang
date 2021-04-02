610bd74fd6a7be086ad7d8cb9785db914a6c7bfe
reisap <r.prasaptaraya@gmail.com>
2021-04-02 23:18:17 +0700

    error handling users

--- handler/user.go
+++ handler/user.go
@@ -19,12 +19,16 @@ func (h *userHandler) RegisterUser(c *gin.Context){
 	var input user.RegisterUserInput
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest,nil)
+		response := helper.APIResponse("Register account Failed",http.StatusBadRequest,"error",nil)
+		c.JSON(http.StatusBadRequest,response)
+		return
 	}
 
 	newUser,err := h.userService.RegisterUser(input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest,nil)
+		response := helper.APIResponse("Register account Failed",http.StatusBadRequest,"error",nil)
+		c.JSON(http.StatusBadRequest,response)
+		return
 	}
 	formatter := user.FormatUser(newUser,"token")
 	response := helper.APIResponse("Account has been registered",http.StatusOK,"success",formatter)
