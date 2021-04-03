875138e0d64f9737bc1482fc69ad61f4f2a5d723
reisap <r.prasaptaraya@gmail.com>
2021-04-03 12:22:12 +0700

    update site Sat Apr  3 12:22:12 WIB 2021

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,28 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID int
+	UserID int
+	Name string
+	ShortDescription string
+	Description string
+	BackerCount int
+	CurrentAmount int
+	GoalAmount int
+	Perks string
+	Slug string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CampaignImages struct {
+	ID int
+	CampaignID int
+	FileName string
+	IsPrimary string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+}
